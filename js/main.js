76e67f82acf9ca739dc1c7cd195a5da9a48aacf0
Yaroslav Oliinyk <iamiaroslav@gmail.com>
2020-07-20 22:54:34 +0300

    main complete

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 window.main = (function () {
   var URL_GET = 'https://javascript.pages.academy/kekstagram/data';
   var URL_POST = 'https://javascript.pages.academy/kekstagram';
+  var AVATAR_SIZE = 35;
   var MAX_COMMENTS_COUNT = 5;
   var HASHTAG_MAX_LENGTH = 20;
   var HASHTAG_MIN_LENGTH = 2;
@@ -15,6 +16,7 @@ window.main = (function () {
   return {
     URL_GET: URL_GET,
     URL_POST: URL_POST,
+    AVATAR_SIZE: AVATAR_SIZE,
     MAX_COMMENTS_COUNT: MAX_COMMENTS_COUNT,
     HASHTAG_MAX_LENGTH: HASHTAG_MAX_LENGTH,
     HASHTAG_MIN_LENGTH: HASHTAG_MIN_LENGTH,
@@ -29,12 +31,6 @@ window.main = (function () {
       var textDescriptionInput = document.querySelector('.text__description');
       if (evt.key === 'Escape' && textHashtagsInput !== document.activeElement && textDescriptionInput !== document.activeElement) {
         evt.preventDefault();
-        action();
-      }
-    },
-
-    isEnterEvent: function (evt, action) {
-      if (evt.key === 'Enter') {
         action();
       }
     }
