c0a1d2543cf0c755a33528b52340e16cdf1c9962
Yaroslav Oliinyk <iamiaroslav@gmail.com>
2020-07-22 23:28:59 +0300

    draft

--- js/preview_copy.js
+++ js/preview_copy.js
@@ -35,39 +35,24 @@
 
   var loadMoreComments = function (commentsArray) {
     var currentCount = window.main.MAX_COMMENTS_COUNT;
-    var moreCount = currentCount + window.main.MAX_COMMENTS_COUNT;
 
     var fragment = document.createDocumentFragment();
 
-    if (moreCount < commentsArray.length) {
-      for (var i = currentCount; i < moreCount; i++) {
-        fragment.appendChild(createCommentElement(commentsArray[i]));
-        socialComments.appendChild(fragment);
-        if (moreCount - i === 1) {
-          currentCount += window.main.MAX_COMMENTS_COUNT;
-        }
-      }
-    } else {
-      for (var j = currentCount; j < commentsArray.length; j++) {
-        fragment.appendChild(createCommentElement(commentsArray[j]));
-        socialComments.appendChild(fragment);
-      }
-      commentsLoader.classList.add('hidden');
+    for (var i = currentCount; i < commentsArray.length; i++) {
+      fragment.appendChild(createCommentElement(commentsArray[i]));
     }
+    socialComments.appendChild(fragment);
+    commentsLoader.classList.add('hidden');
   };
 
 
   var renderComments = function (commentsArray) {
-    var socialCommentCount = document.querySelector('.social__comment-count');
-
     socialComments.innerHTML = '';
     commentsLoader.classList.add('hidden');
 
-    var commentsCount;
+    var commentsCount = commentsArray.length;
 
-    if (commentsArray.length <= window.main.MAX_COMMENTS_COUNT) {
-      commentsCount = commentsArray.length;
-    } else {
+    if (commentsArray.length > window.main.MAX_COMMENTS_COUNT) {
       commentsCount = window.main.MAX_COMMENTS_COUNT;
       commentsLoader.classList.remove('hidden');
     }
@@ -78,54 +63,19 @@
     }
     socialComments.appendChild(fragment);
 
-    if (!socialComments.classList.contains('handler')) {
-      var handle = function () {
-        loadMoreComments(commentsArray);
-      };
-      commentsLoader.addEventListener('click', handle);
-      socialComments.classList.add('handler');
-    }
+    commentsLoader.addEventListener('click', function () {
+      loadMoreComments(commentsArray);
+    });
+
+    // if (!socialComments.classList.contains('handler')) {
+    //   var handle = function () {
+    //     loadMoreComments(commentsArray);
+    //     console.log(commentsArray);
+    //   };
+    //   commentsLoader.addEventListener('click', handle);
+    //   socialComments.classList.add('handler');
+    // }
   };
-
-  // var createComments = function (commentsArray) {
-  //   var commentsLoader = document.querySelector('.comments-loader');
-  //   var socialComments = document.querySelector('.social__comments');
-  //   socialComments.innerHTML = '';
-
-  //   var count;
-
-  //   var fragment = document.createDocumentFragment();
-
-  //   if (commentsArray.length < window.main.MAX_COMMENTS_COUNT) {
-  //     count = commentsArray.length;
-  //     commentsLoader.classList.add('hidden');
-  //   } else {
-  //     count = window.main.MAX_COMMENTS_COUNT;
-  //     commentsLoader.classList.remove('hidden');
-  //     commentsLoader.addEventListener('click', function () {
-  //       var moreCommentsCount = count + window.main.MAX_COMMENTS_COUNT;
-  //       if (moreCommentsCount < commentsArray.length) {
-  //         for (var i = count; i < moreCommentsCount; i++) {
-  //           fragment.appendChild(createCommentElement(commentsArray[i]));
-  //           socialComments.appendChild(fragment);
-  //           if (moreCommentsCount - i === 1) {
-  //             count += window.main.MAX_COMMENTS_COUNT;
-  //           }
-  //         }
-  //       } else {
-  //         for (var j = count; j < commentsArray.length; j++) {
-  //           fragment.appendChild(createCommentElement(commentsArray[j]));
-  //           socialComments.appendChild(fragment);
-  //         }
-  //         commentsLoader.classList.add('hidden');
-  //       }
-  //     });
-  //   }
-  //   for (var i = 0; i < count; i++) {
-  //     fragment.appendChild(createCommentElement(commentsArray[i]));
-  //     socialComments.appendChild(fragment);
-  //   }
-  // };
 
   var changeBigPictureContent = function (pic, commentsArray) {
     var bigPictureImg = document.querySelector('.big-picture__img');
